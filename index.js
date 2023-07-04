51fbe88a42b7bbafc4acd405fa56bdb4c37da2da
tshark2907 <thiagovinicius2907@outlook.com>
2023-07-04 18:01:36 -0300

    Images added

--- index.js
+++ index.js
@@ -7,17 +7,35 @@ botoesSkill.forEach(botao => {
     if (idBotao === 'button_1' || idBotao === 'button_2') {
       const track = document.querySelector('#skilltrack_1');
       if(idBotao === 'button_1'){
-        track.style = 'transform:translateX(-50px);'
+        track.style.transform = 'translateX(55px)';
       }
       if(idBotao === 'button_2'){
-        track.style = 'transform:translateX(50px);'
+        track.style.transform = 'translateX(-55px)';
       }
     } else if (idBotao === 'button_3' || idBotao === 'button_4') {
-      alert('skilltrack_2');
+      const track = document.querySelector('#skilltrack_2');
+      if(idBotao === 'button_3'){
+        track.style.transform = 'translateX(55px)';
+      }
+      if(idBotao === 'button_4'){
+        track.style.transform = 'translateX(-55px)';
+      }
     } else if (idBotao === 'button_5' || idBotao === 'button_6') {
-      alert('skilltrack_3');
+      const track = document.querySelector('#skilltrack_3');
+      if(idBotao === 'button_5'){
+        track.style.transform = 'translateX(55px)';
+      }
+      if(idBotao === 'button_6'){
+        track.style.transform = 'translateX(-55px)';
+      }
     } else if (idBotao === 'button_7' || idBotao === 'button_8') {
-      alert('skilltrack_4');
+      const track = document.querySelector('#skilltrack_4');
+      if(idBotao === 'button_7'){
+        track.style.transform = 'translateX(55px)';
+      }
+      if(idBotao === 'button_8'){
+        track.style.transform = 'translateX(-55px)';
+      }
     }
   });
 });
