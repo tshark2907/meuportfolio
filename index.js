5bf85f6cfcc1d067cb9242ee0b79fe969b9d975c
Thiago Alvarenga <thiagovinicius2907@outlook.com>
2023-07-05 11:43:12 -0300

    Stilization progressed

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let botoesSkill = document.querySelectorAll('.button');
+let containerSize = 55;
+let positions = [0, 0, 0, 0];
 
 botoesSkill.forEach(botao => {
   botao.addEventListener('click', function() {
@@ -6,35 +8,41 @@ botoesSkill.forEach(botao => {
 
     if (idBotao === 'button_1' || idBotao === 'button_2') {
       const track = document.querySelector('#skilltrack_1');
-      if(idBotao === 'button_1'){
-        track.style.transform = 'translateX(55px)';
+      if (idBotao === 'button_2') {
+        if (positions[0] > -(containerSize * (track.childElementCount - 7))) {
+          positions[0] -= containerSize;
+          track.style.transform = `translateX(${positions[0]}px)`;
+        }
       }
-      if(idBotao === 'button_2'){
-        track.style.transform = 'translateX(-55px)';
+      if (idBotao === 'button_1') {
+        if (positions[0] < 0) {
+          positions[0] += containerSize;
+          track.style.transform = `translateX(${positions[0]}px)`;
+        }
       }
     } else if (idBotao === 'button_3' || idBotao === 'button_4') {
       const track = document.querySelector('#skilltrack_2');
-      if(idBotao === 'button_3'){
-        track.style.transform = 'translateX(55px)';
+      if (idBotao === 'button_3') {
+        track.style.transform = `translateX(-${containerSize}px)`;
       }
-      if(idBotao === 'button_4'){
-        track.style.transform = 'translateX(-55px)';
+      if (idBotao === 'button_4') {
+        track.style.transform = `translateX(-${containerSize}px)`;
       }
     } else if (idBotao === 'button_5' || idBotao === 'button_6') {
       const track = document.querySelector('#skilltrack_3');
-      if(idBotao === 'button_5'){
-        track.style.transform = 'translateX(55px)';
+      if (idBotao === 'button_5') {
+        track.style.transform = `translateX(${containerSize}px)`;
       }
-      if(idBotao === 'button_6'){
-        track.style.transform = 'translateX(-55px)';
+      if (idBotao === 'button_6') {
+        track.style.transform = `translateX(-${containerSize}px)`;
       }
     } else if (idBotao === 'button_7' || idBotao === 'button_8') {
       const track = document.querySelector('#skilltrack_4');
-      if(idBotao === 'button_7'){
-        track.style.transform = 'translateX(55px)';
+      if (idBotao === 'button_7') {
+        track.style.transform = `translateX(${containerSize}px)`;
       }
-      if(idBotao === 'button_8'){
-        track.style.transform = 'translateX(-55px)';
+      if (idBotao === 'button_8') {
+        track.style.transform = `translateX(-${containerSize}px)`;
       }
     }
   });
