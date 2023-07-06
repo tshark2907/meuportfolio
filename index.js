c79471d0107d22e88347860f7b7459fae5e37d75
Thiago Alvarenga <thiagovinicius2907@outlook.com>
2023-07-06 12:09:28 -0300

    Stilization progressed, 40%

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let botoesSkill = document.querySelectorAll('.button');
-let containerSize = 55;
+let containerSize = 100;
 let positions = [0, 0, 0, 0];
 
 botoesSkill.forEach(botao => {
@@ -9,7 +9,7 @@ botoesSkill.forEach(botao => {
     if (idBotao === 'button_1' || idBotao === 'button_2') {
       const track = document.querySelector('#skilltrack_1');
       if (idBotao === 'button_2') {
-        if (positions[0] > -(containerSize * (track.childElementCount - 7))) {
+        if (positions[0] > -(containerSize * (track.childElementCount * 2))) {
           positions[0] -= containerSize;
           track.style.transform = `translateX(${positions[0]}px)`;
         }
@@ -23,7 +23,7 @@ botoesSkill.forEach(botao => {
     } else if (idBotao === 'button_3' || idBotao === 'button_4') {
       const track = document.querySelector('#skilltrack_2');
       if (idBotao === 'button_4') {
-        if (positions[1] > -(containerSize * (track.childElementCount - 7))) {
+        if (positions[1] > -(containerSize * (track.childElementCount - 3))) {
           positions[1] -= containerSize;
           track.style.transform = `translateX(${positions[1]}px)`;
         }
@@ -37,7 +37,7 @@ botoesSkill.forEach(botao => {
     } else if (idBotao === 'button_5' || idBotao === 'button_6') {
       const track = document.querySelector('#skilltrack_3');
       if (idBotao === 'button_6') {
-        if (positions[2] > -(containerSize * (track.childElementCount - 7))) {
+        if (positions[2] > -(containerSize * (track.childElementCount - 3))) {
           positions[2] -= containerSize;
           track.style.transform = `translateX(${positions[2]}px)`;
         }
@@ -49,8 +49,9 @@ botoesSkill.forEach(botao => {
         }
       }
     } else if (idBotao === 'button_7' || idBotao === 'button_8') {
+      const track = document.querySelector('#skilltrack_4');
       if (idBotao === 'button_8') {
-        if (positions[3] > -(containerSize * (track.childElementCount - 7))) {
+        if (positions[3] > -(containerSize * (track.childElementCount - 3))) {
           positions[3] -= containerSize;
           track.style.transform = `translateX(${positions[3]}px)`;
         }
